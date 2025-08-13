cacf5979ea18857344db195e553ee44850e20437
E-Leno4ka <egrechman@gmail.com>
2025-08-13 18:34:56 +0300

    добавлено отображение результатов

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-import my_module
+import tkinter as tk
+from my_module import study_entry
 
-print("hello world")
 
-#exit(100)
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Изучение элементов GUI")
+    root.geometry("400x300")
+    tk.Button(root, text="Изучение полей ввода",
+              command=study_entry).pack(pady=20)
+    
+    
+    root.mainloop()
--- my_module.py
+++ my_module.py
@@ -1,9 +1,22 @@
-pi = 3.14
-def summa(a, b) -> int:
-    '''
-    эта функция 
-    делает a + b
-    '''
-    return a + b
+import tkinter as tk
+from tkinter import messagebox
 
-print("Привет из модуля")
+
+def study_entry():
+    window = tk.Toplevel()
+    window.title("Поля ввода")
+    window.geometry("300x200")
+    
+    tk.Label(window, text="Имя:").pack(pady=5)
+    entry1 = tk.Entry(window)
+    entry1.pack(pady=5)
+  
+    tk.Label(window, text="Пароль:").pack(pady=5)
+    entry2 = tk.Entry(window, show="*")
+    entry2.pack(pady=5)
+    
+    def show_values():
+        messagebox.showinfo("Результат", f"Имя: {entry1.get()}\nПароль: {entry2.get()}")
+
+        
+    tk.Button(window, text="Показать", command=show_values).pack(pady=10)
